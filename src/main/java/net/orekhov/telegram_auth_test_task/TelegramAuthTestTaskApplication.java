a65de933d08e18b3e4b8570f016ae49d5e7391d4
Alexander O <orekhov-alexander@rambler.ru>
2025-05-25 08:33:16 +0300

    Initial commit

---
+++ src/main/java/net/orekhov/telegram_auth_test_task/TelegramAuthTestTaskApplication.java
@@ -0,0 +1,13 @@
+package net.orekhov.telegram_auth_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramAuthTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramAuthTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/orekhov/telegram_auth_test_task/TelegramAuthTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package net.orekhov.telegram_auth_test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramAuthTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
